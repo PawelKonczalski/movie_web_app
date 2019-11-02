f1622406aa973a6ee376d244372b56a4a7e545de
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-11-02 15:48:59 +0100

    movie model

---
+++ src/main/java/com/babel/movieapp/movies/model/Movie.java
@@ -0,0 +1,21 @@
+package com.babel.movieapp.movies.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String movieTitle;
+    @ElementCollection
+    private List<MovieCategory> movieCategory;
+    @Column(length = 1024)
+    private String description;
+    private boolean confirmed;
+}
---
+++ src/main/java/com/babel/movieapp/movies/model/MovieCategory.java
@@ -0,0 +1,17 @@
+package com.babel.movieapp.movies.model;
+
+public enum MovieCategory {
+    ACTION,
+    ANIMATION,
+    DOCUMENTARY,
+    DRAMA,
+    FAMILY,
+    FANTASY,
+    HORROR,
+    COMEDY,
+    SHORT,
+    CRIMINAL,
+    ADVENTURE,
+    ROMANCE,
+    SIFI
+}
