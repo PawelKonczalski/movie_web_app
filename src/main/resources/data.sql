0dfa138898b346fe263670129beed4ba7b770a07
PawelKonczalski <50076223+PawelKonczalski@users.noreply.github.com>
2019-11-02 16:03:37 +0100

    test input to check app

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO user (username, password, roles) VALUES ('admin', 'admin', 'ADMIN'), ('user', 'user', 'USER');
+
+INSERT INTO movie (MOVIE_TITLE , DESCRIPTION, CONFIRMED) VALUES
+('Armagedon', 'Po odkryciu asteroidy wielkości Teksasu, która zmierza w kierunku Ziemi, NASA rekrutuje grupę wiertniczych i wysyła na misję ratowania planety.', 'true'),
+('2012', 'Jackson Curtis próbuje ocalić najbliższych przed niszczącymi Ziemię kataklizmami.', 'true'),
+('Czarownica 2', 'Aurora postanawia wyjść za mąż i zamieszkać w zamku przyszłego męża.', 'true'),
+('Boże Ciało', 'Dwudziestoletni Daniel zostaje warunkowo zwolniony z poprawczaka. Wyjeżdża na drugi koniec Polski, żeby pracować w stolarni, ale zamiast tego zaczyna udawać księdza.', 'true'),
+('Jocker', 'Strudzony życiem komik popada w obłęd i staje się psychopatycznym mordercą.', 'false');
+
+INSERT INTO MOVIE_MOVIE_CATEGORY  (MOVIE_ID, MOVIE_CATEGORY ) VALUES
+(1, 0),
+(2, 0),
+(2, 12),
+(3, 4),
+(3, 5),
+(4, 2),
+(5, 3),
+(5, 9);
+
+INSERT INTO USER_MOVIE_LIST   (USER_ID , MOVIE_LIST_ID) VALUES
+(1, 1),
+(1, 2);
